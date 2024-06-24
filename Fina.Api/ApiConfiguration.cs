ec5650426b20bc97299739c7aa174e26078a465a
Alyson Souza Carregosa <125091371+alysonsz@users.noreply.github.com>
2024-06-24 01:17:39 -0300

    Update

--- Fina.Api/ApiConfiguration.cs
+++ Fina.Api/ApiConfiguration.cs
@@ -2,7 +2,7 @@ namespace Fina.Api;
 
 public static class ApiConfiguration
 {
-    public const string UserId = "jornada@balta.io";
+    public const string UserId = "alysonsouza36@gmail.com";
     public static string ConnectionString { get; set; } = string.Empty;
     public static string CorsPolicyName = "wasm";
 }
