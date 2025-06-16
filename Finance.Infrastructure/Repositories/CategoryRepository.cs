acacd94aff70912ee44a94108cf01c03ef4c3487
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-16 16:17:19 -0300

    ✨feat: Implement repository pattern for data access
    This commit provides the concrete implementations for the ICategoryRepository and ITransactionRepository interfaces.
    
    This layer handles all direct database interactions through the AppDbContext. By fulfilling the data access contracts defined in the Domain, this commit completes the data persistence layer, abstracting the "how" of data manipulation from the rest of the application.

---
+++ Finance.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Finance.Domain.Interfaces.Repositories;
+using Finance.Domain.Models;
+using Finance.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Infrastructure.Repositories;
+
+public class CategoryRepository(AppDbContext context) : ICategoryRepository
+{
+    public async Task<Category?> CreateAsync(Category category)
+    {
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category?> UpdateAsync(Category category)
+    {
+        context.Categories.Update(category);
+        await context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category?> DeleteAsync(Category category)
+    {
+        context.Categories.Remove(category);
+        await context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category?> GetByIdAsync(long id, string userId)
+        => await context.Categories
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == id && c.UserId == userId);
+
+    public async Task<List<Category>?> GetAllAsync(string userId)
+        => await context.Categories
+            .AsNoTracking()
+            .Where(c => c.UserId == userId)
+            .OrderBy(c => c.Title)
+            .ToListAsync();
+}
---
+++ Finance.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,46 @@
+﻿using Finance.Domain.Interfaces.Repositories;
+using Finance.Domain.Models;
+using Finance.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Infrastructure.Repositories;
+
+public class TransactionRepository(AppDbContext context) : ITransactionRepository
+{
+    public async Task<Transaction?> CreateAsync(Transaction transaction)
+    {
+        await context.Transactions.AddAsync(transaction);
+        await context.SaveChangesAsync();
+        return transaction;
+    }
+
+    public async Task<Transaction?> UpdateAsync(Transaction transaction)
+    {
+        context.Transactions.Update(transaction);
+        await context.SaveChangesAsync();
+        return transaction;
+    }
+
+    public async Task<Transaction?> DeleteAsync(Transaction transaction)
+    {
+        context.Transactions.Remove(transaction);
+        await context.SaveChangesAsync();
+        return transaction;
+    }
+
+    public async Task<Transaction?> GetByIdAsync(long id, string userId)
+        => await context.Transactions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.Id == id && t.UserId == userId);
+
+    public async Task<List<Transaction>?> GetByPeriodAsync(string userId, DateTime? startDate, DateTime? endDate)
+    {
+        return await context.Transactions
+            .AsNoTracking()
+            .Where(t => t.UserId == userId &&
+                        t.PaidOrReceivedAt >= startDate &&
+                        t.PaidOrReceivedAt <= endDate)
+            .OrderBy(t => t.PaidOrReceivedAt)
+            .ToListAsync();
+    }
+}
