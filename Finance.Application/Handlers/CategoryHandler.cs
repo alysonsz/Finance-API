2bd79759fccb3ff83c3fabf975c3ecbdae9b2f35
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-16 16:20:38 -0300

    ⚙️feat: Implement application use cases in handlers
    This commit provides the concrete implementations for the ICategoryHandler and ITransactionHandler interfaces.
    
    The handlers in the Application layer orchestrate the business logic by using the repository interfaces to interact with the domain entities. They contain the application-specific rules, perform necessary mappings from requests to domain models, and act as the core entry point for the API controllers, thus fulfilling the application's use case contracts.

---
+++ Finance.Application/Handlers/CategoryHandler.cs
@@ -0,0 +1,79 @@
+﻿using Finance.Domain.Interfaces.Handlers;
+using Finance.Domain.Requests.Categories;
+using Finance.Domain.Responses;
+using Finance.Domain.Interfaces.Repositories;
+using Finance.Domain.Models;
+
+namespace Finance.Application.Handlers;
+
+public class CategoryHandler(ICategoryRepository repository) : ICategoryHandler
+{
+    public async Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
+    {
+        var category = new Category { UserId = request.UserId, Title = request.Title, Description = request.Description };
+        try
+        {
+            await repository.CreateAsync(category);
+            return new Response<Category?>(category, 201, "Categoria criada com sucesso!");
+        }
+        catch { return new Response<Category?>(null, 500, "Não foi possível criar a categoria"); }
+    }
+
+    public async Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
+    {
+        try
+        {
+            var category = await repository.GetByIdAsync(request.Id, request.UserId);
+            if (category is null) return new Response<Category?>(null, 404, "Categoria não encontrada");
+
+            category.Title = request.Title;
+            category.Description = request.Description;
+            await repository.UpdateAsync(category);
+
+            return new Response<Category?>(category, 200, "Categoria atualizada com sucesso");
+        }
+        catch { return new Response<Category?>(null, 500, "Não foi possível alterar a categoria"); }
+    }
+
+    public async Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
+    {
+        try
+        {
+            var category = await repository.GetByIdAsync(request.Id, request.UserId);
+            if (category is null) return new Response<Category?>(null, 404, "Categoria não encontrada");
+
+            await repository.DeleteAsync(category);
+            return new Response<Category?>(category, 200, "Categoria excluída com sucesso!");
+        }
+        catch { return new Response<Category?>(null, 500, "Não foi possível excluir a categoria"); }
+    }
+
+    public async Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
+    {
+        try
+        {
+            var category = await repository.GetByIdAsync(request.Id, request.UserId);
+            return category is null
+                ? new Response<Category?>(null, 404, "Categoria não encontrada")
+                : new Response<Category?>(category);
+        }
+        catch { return new Response<Category?>(null, 500, "Não foi possível recuperar a categoria"); }
+    }
+
+    public async Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoriesRequest request)
+    {
+        try
+        {
+            var allCategories = await repository.GetAllAsync(request.UserId);
+
+            if (allCategories == null)
+            {
+                return new PagedResponse<List<Category>?>("Não foi possível obter as categorias", 500);
+            }
+
+            var pagedData = allCategories.Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).ToList();
+            return new PagedResponse<List<Category>?>(pagedData, allCategories.Count, request.PageNumber, request.PageSize);
+        }
+        catch { return new PagedResponse<List<Category>?>("Não foi possível consultar as categorias", 500); }
+    }
+}
---
+++ Finance.Application/Handlers/TransactionHandler.cs
@@ -0,0 +1,109 @@
+﻿using Finance.Domain.Interfaces.Handlers;
+using Finance.Domain.Requests.Transactions;
+using Finance.Domain.Responses;
+using Finance.Domain.Common;
+using Finance.Domain.Enums;
+using Finance.Domain.Interfaces.Repositories;
+using Finance.Domain.Models;
+
+namespace Finance.Application.Handlers;
+
+public class TransactionHandler(ITransactionRepository repository) : ITransactionHandler
+{
+    public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
+    {
+        if (request is { Type: ETransactionType.Withdraw, Amount: >= 0 })
+            request.Amount *= -1;
+
+        var transaction = new Transaction
+        {
+            UserId = request.UserId,
+            CategoryId = request.CategoryId,
+            Title = request.Title,
+            Amount = request.Amount,
+            Type = request.Type,
+            PaidOrReceivedAt = request.PaidOrReceivedAt,
+            CreatedAt = DateTime.Now
+        };
+
+        try
+        {
+            await repository.CreateAsync(transaction);
+            return new Response<Transaction?>(transaction, 201, "Transação criada com sucesso!");
+        }
+        catch
+        {
+            return new Response<Transaction?>(null, 500, "Não foi possível criar sua transação");
+        }
+    }
+
+    public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+    {
+        if (request is { Type: ETransactionType.Withdraw, Amount: >= 0 }) request.Amount *= -1;
+
+        try
+        {
+            var transaction = await repository.GetByIdAsync(request.Id, request.UserId);
+            if (transaction is null) return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+            transaction.CategoryId = request.CategoryId;
+            transaction.Amount = request.Amount;
+            transaction.Title = request.Title;
+            transaction.Type = request.Type;
+            transaction.PaidOrReceivedAt = request.PaidOrReceivedAt;
+
+            await repository.UpdateAsync(transaction);
+            return new Response<Transaction?>(transaction, 200, "Transação atualizada com sucesso!");
+        }
+        catch { return new Response<Transaction?>(null, 500, "Não foi possível atualizar sua transação"); }
+    }
+
+    public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    {
+        try
+        {
+            var transaction = await repository.GetByIdAsync(request.Id, request.UserId);
+            if (transaction is null) return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+            await repository.DeleteAsync(transaction);
+            return new Response<Transaction?>(transaction, 200, "Transação excluída com sucesso!");
+        }
+        catch { return new Response<Transaction?>(null, 500, "Não foi possível excluir sua transação"); }
+    }
+
+    public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+    {
+        try
+        {
+            var transaction = await repository.GetByIdAsync(request.Id, request.UserId);
+            return transaction is null
+                ? new Response<Transaction?>(null, 404, "Transação não encontrada")
+                : new Response<Transaction?>(transaction);
+        }
+        catch { return new Response<Transaction?>(null, 500, "Não foi possível recuperar sua transação"); }
+    }
+
+    public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request)
+    {
+        try
+        {
+            request.StartDate ??= DateTime.Now.GetFirstDay();
+            request.EndDate ??= DateTime.Now.GetLastDay();
+
+            var allTransactions = await repository.GetByPeriodAsync(request.UserId, request.StartDate, request.EndDate);
+
+            if (allTransactions == null)
+            {
+                return new PagedResponse<List<Transaction>?>("Não foi possível obter as transações", 500);
+            }
+
+            var pagedData = allTransactions.Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).ToList();
+
+            return new PagedResponse<List<Transaction>?>(pagedData, allTransactions.Count, request.PageNumber, request.PageSize);
+        }
+        catch
+        {
+            return new PagedResponse<List<Transaction>?>("Não foi possível obter as transações", 500);
+        }
+    }
+}
