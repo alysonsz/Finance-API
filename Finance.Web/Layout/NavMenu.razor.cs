c281f05c28e13c0b837b6d1bebbb0322037a24d6
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-16 16:39:53 -0300

    ✨feat: Implement main application layout & navigation
    This commit adds the `MainLayout` and `NavMenu` components.
    
    It defines the consistent look and feel for the entire application, including the sidebar for navigation and the main content area where all pages will be rendered. This ensures a uniform user experience.

---
+++ Finance.Web/Layout/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Finance.Web.Layout;
+
+public partial class NavMenu
+{
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        _collapseNavMenu = !_collapseNavMenu;
+    }
+}
