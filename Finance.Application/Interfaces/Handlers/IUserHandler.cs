29fb4ea6ee1508f4fca67ee8ca7045afc44a535d
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-18 17:20:56 -0300

    ✨feat: add IUserHandler interface
    - Define contract for user authentication handler with LoginAsync & RegisterAsync methods.

---
+++ Finance.Application/Interfaces/Handlers/IUserHandler.cs
@@ -0,0 +1,10 @@
+﻿using Finance.Application.Requests.Auth;
+using Finance.Application.Responses;
+
+namespace Finance.Application.Interfaces.Handlers;
+
+public interface IUserHandler
+{
+    Task<Response<string>> LoginAsync(LoginRequest request);
+    Task<Response<string>> RegisterAsync(RegisterRequest request);
+}
