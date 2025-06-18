a12df5d644789b81c809a92b36024abeef33b3f5
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-18 17:19:37 -0300

    ✨feat: add LoginRequest & RegisterRequest DTOs
    - Create data transfer objects for user authentication requests in Application layer.

---
+++ Finance.Application/Requests/Auth/LoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Finance.Application.Requests.Auth;
+
+public class LoginRequest
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
---
+++ Finance.Application/Requests/Auth/RegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Finance.Application.Requests.Auth;
+
+public class RegisterRequest
+{
+    public string Name { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
