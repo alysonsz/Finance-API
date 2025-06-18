478ef552576fb88ef38c9755ec75b0a88111a6d4
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-18 17:22:23 -0300

    ✨feat: add IUserRepository interface
    - Define user repository contract for accessing user data by email & adding new users.

---
+++ Finance.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Finance.Domain.Models;
+
+namespace Finance.Application.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+}
