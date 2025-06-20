433deadb01b420fccc66eced57f3094a7c922554
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-06-20 10:16:10 -0300

    🧰feat: add ActionResultExtensions to standardize response output
    - Created FromResponse<T>() extension method to map Response<T> to appropriate IActionResult.
    - Automatically returns 200 OK or 400 BadRequest based on IsSuccess flag.
    - Simplifies and unifies response logic across all controllers.

---
+++ Finance.Api/Extensions/ActionResultExtension.cs
@@ -0,0 +1,20 @@
+﻿using Finance.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.API.Extensions
+{
+    public static class ActionResultExtensions
+    {
+        public static IActionResult FromResponse<T>(this ControllerBase controller, Response<T> response)
+        {
+            return response.IsSuccess
+                ? controller.Ok(response)
+                : controller.BadRequest(new
+                {
+                    status = response.IsSuccess ? 200 : response._code,
+                    message = response.Message,
+                    data = response.Data
+                });
+        }
+    }
+}
